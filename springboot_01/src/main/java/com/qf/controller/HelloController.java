9fbf386821a0c8609876d2bf7f3e95d1746aca39
lituanchuan <945992126@qq.com>
2019-09-27 17:40:56 +0800

    springboot01demo+hllo

--- springboot_01/src/main/java/com/qf/controller/HelloController.java
+++ springboot_01/src/main/java/com/qf/controller/HelloController.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-  @GetMapping(value="/hello")
+
+    @GetMapping(value="/hello")
     public String hell(){
+        System.out.println("hello");
 
         return  "hellogggg";
     }
